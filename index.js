f1a83d2f0c471d0d2fb6310fea1c50043ddfe444
Emmanuel Esan <nikolasdevs@gmail.com>
2023-09-27 18:03:15 +0100

    The home page and other pages were worked on and linked together to fucntion propoerly

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const open = document.getElementById
+const open = document.getElementById("open");
+const aside = document.getElementById("aside");
+const openMain = document.getElementById("open-main");
+const openIcon = document.querySelector(".open i");
+
+open.onclick = function () {
+  aside.classList.toggle("open-aside");
+  openMain.classList.toggle("open-main");
+
+  const isOpen = openMain.classList.contains("open-main");
+
+  openIcon.classList = isOpen ? "bi bi-x" : "bi bi-list";
+};
