0e79c5cb65ec774c70298bfae1045eb4f57bb821
Emmanuel Esan <nikolasdevs@gmail.com>
2023-10-02 15:21:36 +0100

    The nav bar was redesigned with new items, also configured to fit smaller devices

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const aside = document.getElementById("aside");
 const openMain = document.getElementById("open-main");
 const openIcon = document.querySelector(".open i");
 
+const readMores = document.querySelectorAll(".read-more");
+const hTexts = document.querySelectorAll(".hidden-txt");
+
 open.onclick = function () {
   aside.classList.toggle("open-aside");
   openMain.classList.toggle("open-main");
@@ -11,3 +14,20 @@ open.onclick = function () {
 
   openIcon.classList = isOpen ? "bi bi-x" : "bi bi-list";
 };
+
+// Add a click event listener to each "read-more" element
+readMores.forEach(function (readMore, text) {
+  // Add an event listener to each "read-more" element
+  readMore.addEventListener("click", function () {
+    // Get the corresponding "hidden-txt" element for the clicked "read-more"
+    const hText = hTexts[text];
+
+    // Toggle the visibility of the corresponding "hidden-txt" element
+    hText.classList.toggle("hidden-txt");
+
+    // Update the text content of the clicked "read-more" element
+    this.textContent = hText.classList.contains("hidden-txt")
+      ? "READ MORE"
+      : "SHOW LESS";
+  });
+});
